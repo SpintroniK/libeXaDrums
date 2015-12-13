5eb811ac699f4900a34285e0e0473e04933b0d34
Jeremy <jerem.oden@gmail.com>
2015-12-13 15:13:06 +0000

    Added Cymbal class.

---
+++ Source/DrumKit/Instruments/Cymbal.cpp
@@ -0,0 +1,25 @@
+/*
+ * Cymbal.cpp
+ *
+ *  Created on: 13 Dec 2015
+ *      Author: jeremy
+ */
+
+#include "Cymbal.h"
+
+namespace DrumKit
+{
+
+	Cymbal::Cymbal()
+	{
+
+		return;
+	}
+
+	Cymbal::~Cymbal()
+	{
+
+		return;
+	}
+
+}
---
+++ Source/DrumKit/Instruments/Cymbal.h
@@ -0,0 +1,33 @@
+/*
+ * Cymbal.h
+ *
+ *  Created on: 13 Dec 2015
+ *      Author: jeremy
+ */
+
+#ifndef SOURCE_DRUMKIT_INSTRUMENTS_CYMBAL_H_
+#define SOURCE_DRUMKIT_INSTRUMENTS_CYMBAL_H_
+
+#include "Instrument.h"
+
+namespace DrumKit
+{
+
+	class Cymbal: public Instrument
+	{
+
+	public:
+
+		Cymbal();
+		virtual ~Cymbal();
+
+	private:
+
+
+
+	};
+
+}
+
+
+#endif /* SOURCE_DRUMKIT_INSTRUMENTS_CYMBAL_H_ */
