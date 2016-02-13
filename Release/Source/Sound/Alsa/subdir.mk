dbca547ac279c574913e1aa9b083889199c1debb
Jeremy <jerem.oden@gmail.com>
2016-02-13 21:34:18 +0000

    Changed configuration to build in Release mode.

--- Release/Source/Sound/Alsa/subdir.mk
+++ Release/Source/Sound/Alsa/subdir.mk
@@ -4,13 +4,16 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../Source/Sound/Alsa/Alsa.cpp 
+../Source/Sound/Alsa/Alsa.cpp \
+../Source/Sound/Alsa/AlsaParameters.cpp 
 
 OBJS += \
-./Source/Sound/Alsa/Alsa.o 
+./Source/Sound/Alsa/Alsa.o \
+./Source/Sound/Alsa/AlsaParameters.o 
 
 CPP_DEPS += \
-./Source/Sound/Alsa/Alsa.d 
+./Source/Sound/Alsa/Alsa.d \
+./Source/Sound/Alsa/AlsaParameters.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
