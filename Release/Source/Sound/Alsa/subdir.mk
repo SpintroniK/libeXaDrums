2584bf12ff8f1c6bacbbd9a0458ceb997fc5c498
Jeremy <jerem.oden@gmail.com>
2016-03-01 22:56:05 +0000

    Removed libxml2 from the linked libraries.

--- Release/Source/Sound/Alsa/subdir.mk
+++ Release/Source/Sound/Alsa/subdir.mk
@@ -20,7 +20,7 @@ CPP_DEPS += \
 Source/Sound/Alsa/%.o: ../Source/Sound/Alsa/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross G++ Compiler'
-	g++ -std=c++0x -I/usr/include/libxml2 -O3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -std=c++0x -O3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
