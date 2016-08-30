e66e001ffca8978509ca3f069cf9de85fa947b2d
Jeremy <jerem.oden@gmail.com>
2016-08-30 21:12:25 +0100

    Added Metronome class.

---
+++ Source/Metronome/Metronome.cpp
@@ -0,0 +1,112 @@
+/*
+ * Metronome.cpp
+ *
+ *  Created on: 28 Aug 2016
+ *      Author: jeremy
+ */
+
+#include "Metronome.h"
+
+using namespace Sound;
+
+namespace DrumKit
+{
+
+	Metronome::Metronome(AlsaParams alsaParams) : alsaParameters(alsaParams),
+	tempo(120), rhythm(2), beatsPerMeasure(4)
+ 	{
+
+		return;
+	}
+
+	Metronome::~Metronome()
+	{
+
+		return;
+	}
+
+
+	void Metronome::GenerateSine()
+	{
+
+		data.clear();
+
+		// Calculate number of samples to generate the measure
+		float beatsPerSecond = rhythm * float(tempo) / 60.0f;
+		float measureTime = beatsPerMeasure / beatsPerSecond;
+		const int numSamples = alsaParameters.sampleRate * measureTime;
+
+		//
+		//float nPeriods = float(numSamples) / float(alsaParameters.periodSize);
+		//float offset = nPeriods - int(nPeriods);
+		//int correction = offset * std::floor(alsaParameters.periodSize);
+
+		data.resize(numSamples);//std::floor(nPeriods)*alsaParameters.periodSize);
+
+		// Get number of samples between each click
+		float beatsFreq = 1/beatsPerSecond;
+		int beatsRate = std::floor(alsaParameters.sampleRate*beatsFreq);
+
+		// Sinusoid parameters
+		float fSineHz = 880.0f;
+		float radiansPerSample = fSineHz * 2*M_PI / alsaParameters.sampleRate;
+		float clickDuration = 10.0f/1000.0f;
+		std::size_t clickSamples = std::floor(alsaParameters.sampleRate*clickDuration);
+
+		int n = 0;
+		int mul = 1;
+		std::size_t click;
+		double phase = 0;
+		short amplitude = std::numeric_limits<short>::max();
+
+		for(std::size_t i = 0; i < data.size(); i++)
+		{
+
+			click = n * beatsRate;// + correction;
+
+	        if(i > click && i < click + clickSamples)
+	        {
+
+	        	if(n % beatsPerMeasure == 0)
+	        	{
+	        		mul = 2;
+	        	}
+	        	else
+	        	{
+	        		mul = 1;
+	        	}
+
+	        	phase  		+= mul*radiansPerSample;
+	        	data[i] 	 = (short)(float(amplitude) * std::sin(phase));
+
+	        }
+	        else  // No click, so signal is zero
+	        {
+
+	        	data[i] = 0;
+	        	phase = 0;
+
+	        	if(i == click + clickSamples)
+	        	{
+	        		n++;
+	        	}
+	        }
+
+		}
+
+		return;
+	}
+
+
+	void Metronome::SetTempo(int t)
+	{
+
+		tempo = t;
+
+		if(tempo >= 250) tempo = 250;
+		if(tempo <= 40) tempo = 40;
+
+		return;
+	}
+
+} /* namespace DrumKit */
---
+++ Source/Metronome/Metronome.h
@@ -0,0 +1,53 @@
+/*
+ * Metronome.h
+ *
+ *  Created on: 28 Aug 2016
+ *      Author: jeremy
+ */
+
+#ifndef SOURCE_METRONOME_METRONOME_H_
+#define SOURCE_METRONOME_METRONOME_H_
+
+#include "../Sound/Alsa/Alsa.h"
+
+#include <vector>
+#include <limits>
+
+#include <cmath>
+
+#define _USE_MATH_DEFINES
+
+namespace DrumKit
+{
+
+	class Metronome
+	{
+
+	public:
+
+		Metronome(Sound::AlsaParams alsaParams);
+		virtual ~Metronome();
+
+		void GenerateSine();
+
+		std::vector<short> GetData() const { return data; }
+		int GetTempo() const { return tempo; }
+
+		void SetTempo(int tempo);
+
+	private:
+
+		Sound::AlsaParams alsaParameters;
+
+		// Metronome parameters
+		int tempo;
+		int rhythm;
+		int beatsPerMeasure;
+
+		std::vector<short> data;
+
+	};
+
+} /* namespace DrumKit */
+
+#endif /* SOURCE_METRONOME_METRONOME_H_ */
