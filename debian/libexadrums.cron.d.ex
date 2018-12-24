92ef4961ff8987f07ecdcff182ef403139a24b99
Jeremy Oden <jerem.oden@gmail.com>
2018-12-24 15:24:32 +0000

    Updated debian package build scripts.

--- debian/libexadrums.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the libexadrums package
-#
-0 4	* * *	root	[ -x /usr/bin/libexadrums_maintenance ] && /usr/bin/libexadrums_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(libexadrums):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="libexadrums" command="/usr/bin/libexadrums"
